3aa5181d026703b17438f6e6e018c26915dbf9df
Shermam Miranda <shermam.miranda@hotmail.com>
2018-09-28 13:49:35 -0300

    tratamento de sufixo

--- src/renderInterface.js
+++ src/renderInterface.js
@@ -18,7 +18,7 @@ module.exports = function (namespace) {
                 .Select(
                 reg => new
                 {
-${entity.Properties.map(renderPropertyInExport)
+${entity.Properties.map(prop => renderPropertyInExport(prop))
                 .filter(line => line)
                 .join('\n')}
                 }).ToList();
@@ -33,9 +33,7 @@ ${entity.Properties.map(renderPropertyInExport)
     }
 }
 
-function renderPropertyInExport(prop) {
-
-
+function renderPropertyInExport(prop, preffix = "", suffix = "") {
     if (prop.isPrimaryKey) {
         return '';
     }
@@ -52,64 +50,23 @@ function renderPropertyInExport(prop) {
     // Percorre as chaves da tabela referenciada.
     return prop.referedEntity.uniqueKey.properties.map(refProp => {
         if (refProp.referedEntity) {
-            // return `\t\t\t\t\treg.${prop.referedEntity.className}.${refProp.referedEntity.className},`
-            return '';
+            return renderPropertyInExport(refProp, (preffix ? `${preffix}.` : "") + refProp.entity.className, (prop.suffix || suffix));
         }
 
-        if (prop.isNullable) {
-            if (refProp.type.nullability === "questionMark") {
-                return `\t\t\t\t\t${prop.referedEntity.className}_${refProp.propertyName + (prop.suffix || '')} = (${refProp.type.csharpType}?) reg.${prop.referedEntity.className + (prop.suffix || '')}.${refProp.propertyName},`;
-            } else {
-                return `\t\t\t\t\t${prop.referedEntity.className}_${refProp.propertyName + (prop.suffix || '')} = reg.${prop.referedEntity.className + (prop.suffix || '')}.${refProp.propertyName},`;
-                // return `\t\t\t\t\t${refProp.propertyName} = reg.${prop.propertyName} != null ? reg.${prop.referedEntity.className}.${refProp.propertyName} : null,`;
-            }
-        }
-
-        return `\t\t\t\t\t${prop.referedEntity.className}_${refProp.propertyName + (prop.suffix || '')} = reg.${prop.referedEntity.className + (prop.suffix || '')}.${refProp.propertyName},`;
+        return generateUkProperty(preffix, prop, refProp, suffix);
     }).filter(line => line).join('\n');
 }
 
-function renderConstants(constants, property) {
+function generateUkProperty(preffix, prop, refProp, suffix) {
+    let ret = "";
+    ret += "\t\t\t\t\t";
+    ret += preffix ? `${preffix.replace(/\./g, "_")}_` : "";
+    ret += prop.referedEntity.className + "_";
+    ret += refProp.propertyName + (prop.suffix || '') + suffix + " = ";
+    ret += prop.isNullable && refProp.type.nullability === "questionMark" ? `(${refProp.type.csharpType}?) ` : "";
+    ret += `reg.${preffix ? `${preffix}.` : ""}`;
+    ret += prop.referedEntity.className + (prop.suffix || '') + ".";
+    ret += refProp.propertyName + ",";
 
-    if (!property.listOfValues) return constants;
-
-    constants = constants.concat(property.listOfValues.map(value => {
-        const quotedValue = property.type.csharpType === "string" ? `"${value.code}"` : value.code;
-        return `\t\tpublic const ${property.type.csharpType} ${property.constantName + "_" + value.constantName} = ${quotedValue};`;
-    }));
-
-    return constants;
-}
-
-function renderProperty(c) {
-    return [
-        `${c.isPrimaryKey ? '[Key]' : ''}`,
-        `${c.isIdentity ? '[DatabaseGenerated(DatabaseGeneratedOption.Identity)]' : `${c.isPrimaryKey ? '[DatabaseGenerated(DatabaseGeneratedOption.None)]' : ''}`}`,
-        `${!c.isNullable && c.type.nullability === 'annotation' ? '[Required]' : ''}`,
-        `${c.type.lengthInfo && c.size > 0 ? `[${c.type.lengthInfo}(${c.size})]` : ''}`,
-        `[Column("${c.columnName}"${c.type.typeName ? `, TypeName = "${c.type.typeName}"` : ''}${c.order ? `, Order = ${c.order - 1}` : ''})]`,
-        `public ${c.type.csharpType}${c.isNullable && c.type.nullability === 'questionMark' ? '?' : ''} ${c.propertyName} { get; set; }`
-    ].filter(line => line)
-        .map(line => '\t\t' + line)
-        .join('\n');
-}
-
-function renderInRelation(r) {
-    return [
-        `[ForeignKey("${r.column}")]`,
-        `public virtual ${r.refClass} ${r.refProp} { get; set; }`
-    ].map(line => '\t\t' + line)
-        .join('\n');
-}
-
-function renderOutRelation(r) {
-    return [
-        `[ForeignKey("${r.column}")]`,
-        `public virtual ICollection<${r.class}> List${r.prop} { get; set; }`
-    ].map(line => '\t\t' + line)
-        .join('\n');
-}
-
-function renderOutRelationInit(r) {
-    return `\t\t\tList${r.prop} = new HashSet<${r.class}>();`;
+    return ret;
 }
