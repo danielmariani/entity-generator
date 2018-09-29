c2346930da01fcde46b9a0e18a9a202e7c8e0e00
Shermam Miranda <shermam.miranda@hotmail.com>
2018-09-29 16:27:38 -0300

    ajustes do import

--- src/renderInterface.js
+++ src/renderInterface.js
@@ -44,6 +44,10 @@ ${((entity.uniqueKey || {}).properties || []).map(prop => renderUkDicWhere(prop)
 
         public override ${entity.className} GetByUnique(${entity.className} entity)
         {
+${((entity.uniqueKey || {}).properties || []).map(prop => renderUkObjDeclaration(prop))
+                .filter(line => line)
+                .join('\n')}
+
             return db.${entity.className}
 ${((entity.uniqueKey || {}).properties || []).map(prop => renderUkObjWhere(prop))
                 .filter(line => line)
@@ -69,46 +73,42 @@ ${entity.Properties.map(prop => renderPropertyInImport(prop))
     }
 }
 
+function renderUkObjDeclaration(prop, preffix = "") {
+    if (prop.referedEntity && prop.referedEntity.uniqueKey) {
+        return prop.referedEntity.uniqueKey.properties.map(refProp => renderUkObjDeclaration(refProp, `${preffix}.${refProp.entity.className}`))
+            .filter(line => line)
+            .join("\n");
+    }
+
+    return `\t\t\t${prop.type.csharpType} ${prop.propertyName} = entity${preffix}.${prop.propertyName};`;
+}
+
 function renderPropertyInImport(prop) {
     if (prop.referedEntity) {
-        return `\t\t\tentity.${prop.referedEntity.className} = (new Interface${prop.referedEntity.className}()).GetByUnique(GenDic(dic, new HashSet<string>() { ${renderRecursiveProperties(prop)} }));`;
+        return `\t\t\tentity.${prop.referedEntity.className + (prop.suffix || "")} = (new Interface${prop.referedEntity.className}(db)).GetByUnique(GenDic(dic, new HashSet<string>() { ${renderRecursiveProperties(prop, "", (prop.suffix || ""))} }));`;
+    }
+
+    if (["decimal", "bool"].includes(prop.type.csharpType)) {
+
+        if (prop.isNullable && prop.type.nullability === "questionMark") {
+            return `\t\t\tentity.${prop.propertyName} = dic["${prop.propertyName}"] is null ? null : (${prop.type.csharpType}?)${prop.type.csharpType}.Parse(dic["${prop.propertyName}"].ToString());`;
+        }
+
+        return `\t\t\tentity.${prop.propertyName} = ${prop.type.csharpType}.Parse(dic["${prop.propertyName}"].ToString());`;
     }
 
     return `\t\t\tentity.${prop.propertyName} = (${prop.type.csharpType})dic["${prop.propertyName}"];`;
 }
 
-function renderRecursiveProperties(prop) {
-    // "CadPosto_CadUnidade_CadComplexo_CadDiretoria_SglDiretoria", "CadPosto_CadUnidade_CadComplexo_SglComplexo", "CadPosto_CadUnidade_SglUnidade", "CadPosto_SglPosto"
-
-    if (prop.referedEntity) {
-        return renderRecursiveProperties(prop);
+function renderRecursiveProperties(prop, preffix = "", suffix = "") {
+    if (prop.referedEntity && prop.referedEntity.uniqueKey) {
+        return prop.referedEntity.uniqueKey.properties
+            .map(refProp => renderRecursiveProperties(refProp, preffix + `${refProp.entity.className}_`, suffix))
+            .join(", ");
     }
 
-    return `"${prop.propertyName}"`;
+    return `"${preffix}${prop.propertyName}${suffix}"`;
 }
-
-// entity.CadPosto = (new InterfaceCadPosto()).GetByUnique(GenDic(dic, new HashSet<string>() {  }));
-// entity.CadUsuario = (new InterfaceCadUsuario()).GetByUnique(GenDic(dic, new HashSet<string>() { "CadUsuario_CodUsuario" }));
-// entity.CadEquipamento = (new InterfaceCadEquipamento()).GetByUnique(GenDic(dic, new HashSet<string>() { "CadEquipamento_CodTagPlaca", "CadEquipamento_CadComplexo_CadDiretoria_SglDiretoria", "CadEquipamento_CadComplexo_SglComplexo" }));
-// entity.RegTurno = (new InterfaceRegTurno()).GetByUnique(GenDic(dic, new HashSet<string>() { "RegTurno_DthInicial", "RegTurno_CadPosto_CadUnidade_CadComplexo_CadDiretoria_SglDiretoria", "RegTurno_CadPosto_CadUnidade_CadComplexo_SglComplexo", "RegTurno_CadPosto_CadUnidade_SglUnidade", "RegTurno_CadPosto_SglPosto" }));
-// entity.CadCentroCusto = (new InterfaceCadCentroCusto()).GetByUnique(GenDic(dic, new HashSet<string>() { "CadCentroCusto_CodCentroCusto" }));
-// entity.CadOrdemEstatistica = (new InterfaceCadOrdemEstatistica()).GetByUnique(GenDic(dic, new HashSet<string>() { "CadOrdemEstatistica_CodOrdemEstatistica" }));
-// entity.CadElementoPep = (new InterfaceCadElementoPep()).GetByUnique(GenDic(dic, new HashSet<string>() { "CadElementoPep_CodElementoPep" }));
-// entity.CadEmpresa = (new InterfaceCadEmpresa()).GetByUnique(GenDic(dic, new HashSet<string>() { "CadEmpresa_CnpjEmpresa" }));
-// entity.CadContrato = (new InterfaceCadContrato()).GetByUnique(GenDic(dic, new HashSet<string>() { "CadContrato_CodContrato" }));
-// entity.CadGri = (new InterfaceCadGri()).GetByUnique(GenDic(dic, new HashSet<string>() { "CadGri_CodGri" }));
-// entity.CadTipoFiscal = (new InterfaceCadTipoFiscal()).GetByUnique(GenDic(dic, new HashSet<string>() { "CadTipoFiscal_DscTipoFiscal" }));
-// entity.DthInicial = (DateTime)dic["DthInicial"];
-// entity.DthFinal = (DateTime)dic["DthFinal"];
-// entity.ValHorHod = (decimal)dic["ValHorHod"];
-// entity.ChkResetAutomaticoHorHod = (bool)dic["ChkResetAutomaticoHorHod"];
-// entity.ChkHorimetro = dic["ChkHorimetro"] is null ? null : (bool?)dic["ChkHorimetro"];
-// entity.SglGerenciaGa = (string)dic["SglGerenciaGa"];
-// entity.NomDispositivo = (string)dic["NomDispositivo"];
-// entity.DthCriacaoReg = (DateTime)dic["DthCriacaoReg"];
-// entity.StuAbastecimento = (string)dic["StuAbastecimento"];
-// entity.TransferenciaProduto = (new InterfaceTransferenciaProduto()).GetByUnique(GenDic(dic, new HashSet<string>() { "TransferenciaProduto_DataHoraUTCInicioRetiradaOrigem" }));
-
 
 function renderUkObjWhere(prop, preffix = "") {
     if (prop.referedEntity && prop.referedEntity.uniqueKey) {
@@ -117,7 +117,7 @@ function renderUkObjWhere(prop, preffix = "") {
             .join("\n");
     }
 
-    return `\t\t\t\t.Where(item => item${preffix}.${prop.propertyName} == entity${preffix}.${prop.propertyName})`;
+    return `\t\t\t\t.Where(item => item${preffix}.${prop.propertyName} == ${prop.propertyName})`;
 }
 
 function renderUkDicWhere(prop, preffix = "") {
